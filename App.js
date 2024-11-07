d2f662014ec6f39304745ca2bdb4e6af2973f825
Anurag Bansal <nb.anurag@gmail.com>
2024-11-07 14:49:21 +0530

    episode 2 code

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement('h1', {id: 'heading', xyz:'abc'}, 'hello anurag');
 const root = ReactDOM.createRoot(document.getElementById("root")); // Create a root
 root.render(heading); // Use root.render instead of ReactDOM.render
